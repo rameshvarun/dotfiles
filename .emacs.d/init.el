977a136b1c3b7b6f6e720aae3650bcacf6eea7a4
Varun Ramesh <varunramesh@fb.com>
2015-07-01 15:00:41 -0700

    Dismiss emacs startup screen

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,19 @@
-; Window navigation
+
+;; Window navigation
 (global-set-key (kbd "C-c <left>")  'windmove-left)
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
