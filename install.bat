1c09df0ef8d768facbc7a969113db311a861ba73
Varun Ramesh <varunramesh@live.com>
2015-07-08 23:07:14 -0700

    Windows install script

---
+++ install.bat
@@ -0,0 +1,6 @@
+mklink /d "%UserProfile%\vimfiles" "%cd%\.vim"
+mklink "%UserProfile%\.vimrc" "%cd%\.vimrc"
+
+mklink "%UserProfile%\.aliases" "%cd%\.aliases"
+
+mklink /d "%UserProfile%\.emacs.d" "%cd%\.emacs.d"
