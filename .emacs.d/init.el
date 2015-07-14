9997c25513107e20f268fc85c539736437118c13
Varun Ramesh <varunramesh@live.com>
2015-07-14 00:43:27 -0700

    Emacs default to wombat theme.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat)))
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
