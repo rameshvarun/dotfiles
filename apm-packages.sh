59c940e681d71cbc57caea210aed640d5fa84106
Varun Ramesh <varunramesh@live.com>
2017-03-14 07:50:18 -0700

    Added a bunch of snippets

--- apm-packages.sh
+++ apm-packages.sh
@@ -1,12 +1,13 @@
 # This is simply a list of Atom packages I like to keep installed, in order to
 # bootstrap my environment on new computers.
 
-apm install language-lua
-
 apm install autocomplete-python # Intelligent auto-complete using Jedi
 apm install file-icons # Lots of cool file icons.
 apm install pigments # Highlight colors found in code.
 
+apm install linter # UI for linters.
 
-apm install linter
+apm install language-lua
 apm install linter-luaparse # Lua linter that doesn't require Lua to be installed.
+
+apm install make-executable
