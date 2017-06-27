c4e3cb99d09aed898e0c6230b5b7d4504d348957
Varun Ramesh <varunramesh@fb.com>
2017-06-27 14:18:03 -0700

    Some basic zsh plugins.

---
+++ init.zsh
@@ -0,0 +1,14 @@
+zplug "plugins/git",   from:oh-my-zsh # Git integration
+zplug "zsh-users/zsh-syntax-highlighting", defer:2 # Syntax highlighting
+zplug 'dracula/zsh', as:theme # Dracula theme
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load --verbose
