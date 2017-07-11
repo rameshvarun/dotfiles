92abbfe986af50094bda8bc7e72415bc11b734a3
Varun Ramesh <varunramesh@fb.com>
2017-07-11 11:09:18 -0700

    Added zsh-autosuggestions + put zplug as submodule.

--- init.zsh
+++ init.zsh
@@ -1,4 +1,4 @@
-# Save history
+# History Options
 export HISTFILE="$HOME/.zsh_history"
 export HISTSIZE=10000
 export SAVEHIST=10000
@@ -6,8 +6,13 @@ setopt EXTENDED_HISTORY
 setopt INC_APPEND_HISTORY
 setopt HIST_IGNORE_DUPS
 
+# Load ZPlug
+export ZPLUG_HOME=~/dotfiles/zplug
+source $ZPLUG_HOME/init.zsh
+
 zplug "plugins/git",   from:oh-my-zsh # Git integration
 zplug "zsh-users/zsh-syntax-highlighting", defer:2 # Syntax highlighting
+zplug "zsh-users/zsh-autosuggestions", defer:3 # Autosuggest on type (like fish shell)
 zplug 'dracula/zsh', as:theme # Dracula theme
 
 # Install plugins if there are plugins that have not been installed
