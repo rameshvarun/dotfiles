482fccaf6e015d1a396593c53ec5c1a110b637e4
Varun Ramesh <varun@gridspace.com>
2024-02-05 07:56:38 -0800

    Zellij shell aliases

--- init.sh
+++ init.sh
@@ -30,6 +30,13 @@ elif type "exa" &> /dev/null; then
   alias ls="exa"
 fi
 
+if type "zellij" &> /dev/null; then
+  function zr () { zellij run --name "$*" -- bash -ic "$*";}
+  function zrf () { zellij run --name "$*" --floating -- bash -ic "$*";}
+  function ze () { zellij edit "$*";}
+  function zef () { zellij edit --floating "$*";}
+fi
+
 # Interactive CD using FZF.
 function fzcd {
   cd ./$(find * -type d | fzf)
