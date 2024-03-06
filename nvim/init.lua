d22a748ae7ee2c3ed82d62a7aebd23b8711d1cdc
Varun Ramesh <varun@gridspace.com>
2024-03-06 07:53:49 -0700

    Use nerd font when available

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,24 +12,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Check if NerdFont env variable is set.
+local nerd_font = os.getenv("NERD_FONT") == "1"
+
 require("lazy").setup({
 	{"folke/tokyonight.nvim"},
 	{"catppuccin/nvim"},
 	{ "nvim-lua/plenary.nvim" },
 	{ "MunifTanjim/nui.nvim"},
-	{"nvim-neo-tree/neo-tree.nvim", branch = "v3.x",
-	config=function()
-		require("neo-tree").setup({
-			default_component_configs = {
-				icon = {
-					enabled = false
-				},
-				git_status = {
-					symbols = false
-				}
-			}
-		})
-	end},
+	{"nvim-neo-tree/neo-tree.nvim", branch = "v3.x"},
 	{'nvim-telescope/telescope.nvim', tag = '0.1.5'},
 	{'akinsho/bufferline.nvim'},
 	{'nvim-lualine/lualine.nvim'},
@@ -56,19 +47,32 @@ vim.cmd[[colorscheme catppuccin-mocha]]
 
 require('gitsigns').setup()
 
-require("bufferline").setup({
-	options={
-		show_buffer_icons=false,
-		buffer_close_icon='×',
-	}
-})
+if nerd_font then
+	require("neo-tree").setup()
+	require("bufferline").setup()
+	require('lualine').setup()
+else
+	require("neo-tree").setup({
+		default_component_configs = {
+			icon = { enabled = false },
+			git_status = { symbols = false }
+		}
+	})
 
-require('lualine').setup({
-	options = {
-		icons_enabled = false,
-		section_separators = '', component_separators = ''
-	}
-})
+	require("bufferline").setup({
+		options={
+			show_buffer_icons=false,
+			buffer_close_icon='×',
+		}
+	})
+
+	require('lualine').setup({
+		options = {
+			icons_enabled = false,
+			section_separators = '', component_separators = ''
+		}
+	})
+end
 
 local cmp = require('cmp');
 cmp.setup {
