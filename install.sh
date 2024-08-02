ff605cda8fb72353b8bf53e1bd3abc00728d4889
Varun Ramesh <varunramesh@live.com>
2024-08-01 21:11:23 -0700

    Remove unused parts of install script.

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-# Vi mode in GNU Readline
-ln -s $PWD/.inputrc ~/.inputrc
-
 # Vim files
 echo "source ~/dotfiles/.vimrc" >> ~/.config/.vimrc
 
 # Bash
 echo "source ~/dotfiles/init.sh" >> ~/.bashrc
-
-# Neovim files
-ln -s $PWD/nvim/ ~/.config/nvim
 
 # Zellij Configuration
 ln -s $PWD/zellij/ ~/.config/zellij
@@ -29,12 +23,3 @@ ln -s $PWD/.wezterm.lua ~/.wezterm.lua
 
 # Emacs files
 ln -s $PWD/.emacs.d ~/.emacs.d
-
-# Mjolnir
-ln -s $PWD/.mjolnir ~/.mjolnir
-
-# Aliases
-ln -s $PWD/.aliases ~/.aliases
-
-# Atom snippets
-ln -s $PWD/snippets.cson ~/.atom/snippets.cson
